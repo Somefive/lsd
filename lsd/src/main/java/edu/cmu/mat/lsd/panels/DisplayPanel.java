573305721003d85ee7933c936702e5748938621a
Somefive <Somefive@foxmail.com>
2017-08-22 05:30:59 +0800

    remove rubbish code

--- lsd/src/main/java/edu/cmu/mat/lsd/panels/DisplayPanel.java
+++ lsd/src/main/java/edu/cmu/mat/lsd/panels/DisplayPanel.java
@@ -33,8 +33,7 @@ import edu.cmu.mat.scores.Section;
 import edu.cmu.mat.scores.System;
 
 public class DisplayPanel implements Panel, HcmpListener {
-	private static final int PAGE_LEFT = 20;
-
+	
 	private Model _model;
 	private Score _score;
 
@@ -42,8 +41,6 @@ public class DisplayPanel implements Panel, HcmpListener {
 	// arrangement is handled. This is because it is complicated to update the
 	// rendering with new sizes with how the code is currently setup.
 	private JScrollPane _scroller;
-	private int _height;
-	private int _width;
 
 	private double _scale = 1.0;
 	public double getScale() { return _scale; }
@@ -56,19 +53,14 @@ public class DisplayPanel implements Panel, HcmpListener {
 	private JBlock _upper_block;
 	private JBlock _lower_block;
 	
-	private boolean _is_arrow_visible = false;
-	private boolean _is_line_visible = false;
-
 	private List<Block> _blocks;
-	private Barline _previous_jump_to = null;
 
 	private TimeMap _time_map;
 	private Timer _play_timer;
 
 	private int _playback_id = 0;
-	private List<PlaybackEvent> _playback_events = new ArrayList<PlaybackEvent>();
+	private List<PlaybackEvent> _playback_events = new ArrayList<>();
 	private int _events_index = 0;
-	private int _current_block_index = 0;
 	private int _event_index_to_be_drawn = 0;
 	public int getEventIndexToBeDrawn() { return _event_index_to_be_drawn; }
 	public PlaybackEvent getCurrentPlaybackEventToBeDrawn() {
@@ -77,10 +69,6 @@ public class DisplayPanel implements Panel, HcmpListener {
 				null;
 	}
 	
-	public static int TO = 0;
-	public static int FROM = 1;
-	public static int LENGTH = 2;
-
 	public DisplayPanel(Model model) {
 		_model = model;
 		_model.getHcmp().setListener(this);
@@ -166,10 +154,8 @@ public class DisplayPanel implements Panel, HcmpListener {
 			int end_beat = end_barlines.indexOf(section.getEnd()) * 4;
 			int duration = end_beat - start_beat;
 			message_parts.add( name + "," + (start_beat + offset) + ","
-					+ duration /*+ '"'*/);
+					+ duration );
 		}
-//		for (int i=0; i<arrangement.size();i++)
-//			java.lang.System.out.println(message_parts.get(i));
 		return(message_parts);
 	}
 	
@@ -199,15 +185,6 @@ public class DisplayPanel implements Panel, HcmpListener {
 			}
 				_scroller.revalidate();
 				_scroller.repaint();
-			/*
-				java.lang.System.out.println("test");
-				
-				Set<Section> sections =  _score.getSections();
-				for (Section section: sections) {
-					if (section.getName() == null)
-						_score.removeSection(section);
-				}
-				*/
 			}
 			else {
 				handleStop();
@@ -268,12 +245,6 @@ public class DisplayPanel implements Panel, HcmpListener {
 		_playback_events = new_events;
 		_blocks = _score.createBlockList(_playback_events, 0.5);
 		
-		_current_block_index = 0;
-		_is_arrow_visible = false;
-		_is_line_visible = false;
-
-//		_panel.removeAll();
-
 		initializeBlocks(0);
 
 		int width = _scroller.getWidth();
@@ -317,8 +288,6 @@ public class DisplayPanel implements Panel, HcmpListener {
 	}
 
 	private void initializeBlocks(int index) {
-		_height = _scroller.getHeight();
-		_width = _scroller.getWidth();
 		int blockHeight = (_scroller.getHeight() - 20) / 2;
 		_upper_block = new JBlock(blockHeight);
 		_lower_block = new JBlock(blockHeight);
@@ -358,27 +327,11 @@ public class DisplayPanel implements Panel, HcmpListener {
 		}
 
 		delay = Math.max(delay, 0);
-		_play_timer = new Timer((int) delay, new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-//				if (_events_index < _playback_events.size())
-//					updateBlock();
-				redraw();
-//				drawArrow();
-//				moveCursor();
-				
-				int playBackEventIndex = Math.min(_playback_events.size()-1, _events_index);
-				PlaybackEvent playbackEvent = _playback_events.get(playBackEventIndex);
-				Barline barline = _events_index < _playback_events.size() ? playbackEvent.getStart() : playbackEvent.getEnd();
-				int nextPlayBackEventIndex = Math.min(_playback_events.size()-1, _events_index+1);
-				PlaybackEvent nextPlaybackEvent = _playback_events.get(nextPlayBackEventIndex);
-				Barline nextBarline = (_events_index+1) < _playback_events.size() ?
-						nextPlaybackEvent.getStart() : nextPlaybackEvent.getEnd();
-				DisplayServer.broadcast(new BeatMessage(_events_index));
-				
-				if (_events_index < _playback_events.size()) {
-					fireNextEvent(id);
-				}
+		_play_timer = new Timer((int) delay, arg0 -> {
+			redraw();
+			DisplayServer.broadcast(new BeatMessage(_events_index));
+			if (_events_index < _playback_events.size()) {
+				fireNextEvent(id);
 			}
 		});
 
@@ -401,174 +354,7 @@ public class DisplayPanel implements Panel, HcmpListener {
 		}
 		moveCursor();
 	}
-
-	private void updateBlock() {
-		Block current_block = _blocks.get(_current_block_index);
-		if (_current_block_index < _blocks.size() - 1) {
-			if (_score.outOfBlock(current_block,
-					_playback_events.get(_events_index).getStart())) {
-				_current_block_index += 1;
-				if (_score.outOfBlock(_blocks.get(_current_block_index),
-						_playback_events.get(_events_index).getStart())) {
-					java.lang.System.err
-							.print("Error! Current event out of current and next block.\n");
-				}
-			}
-		}
-		current_block = _blocks.get(_current_block_index);
-		if (!current_block.isBlockFlipped()) {
-			if (_current_block_index < _blocks.size() - 1) {
-				Block next_block = _blocks.get(_current_block_index + 1);
-				if (_current_block_index % 2 == 0) {
-					_lower_block.setBlock(next_block);
-				} else {
-					_upper_block.setBlock(next_block);
-				}
-
-				current_block.flipToNextBlock();
-			} else {
-				current_block.flipToNextBlock();
-			}
-		}
-		redraw();
-	}
-
-	private boolean isCloseTo(int d, int current_events_index, Barline to) {
-		int end = current_events_index + d;
-		if (end >= _playback_events.size()) {
-			end = _playback_events.size() - 1;
-		}
-		for (int i = current_events_index; i < end; i++) {
-			if (_playback_events.get(i).getEnd() == to) {
-				return true;
-			}
-		}
-		return false;
-	}
 	
-	private int cal(Barline next_jump_to, System current_system, int from_y, int to_y, int type) {
-		
-		if (from_y < to_y) {
-			to_y -= next_jump_to.getParent().getInnerHeight();
-		} else if (from_y > to_y) {
-			from_y -= current_system.getInnerHeight();
-		}
-		
-		if (type == LENGTH) return java.lang.Math.abs(from_y - to_y);
-		if (type == FROM) return from_y;
-		return to_y;
-	}
-
-	private void drawArrow() {
-		if (_events_index < _playback_events.size()) {
-			PlaybackEvent current_event = _playback_events.get(_events_index);
-			System current_system = current_event.getStart().getParent();
-			Block current_block = _blocks.get(_current_block_index);
-			Barline next_jump_from = current_block.getNextJumpFrom();
-			Barline next_jump_to = current_block.getNextJumpTo();
-
-			if (_previous_jump_to != null
-					&& current_event.getStart() == _previous_jump_to) {
-				_is_arrow_visible = false;
-				_is_line_visible = false;
-			}
-
-			if (!_is_arrow_visible) {
-				// show arrow on the system
-				// if (next_jump_from != null && current_system ==
-				// next_jump_from.getParent())
-
-				// show arrow before 2 measures
-				if (next_jump_from != null && next_jump_to != null
-						&& isCloseTo(2, _events_index, next_jump_from)) {
-					_is_arrow_visible = true;
-					_is_line_visible = true;
-
-					int from_x = (int) (next_jump_from.getOffset() * getJBlock(
-							true).getImageWidth())
-							+ getJBlock(true).getImageOffset() - PAGE_LEFT;
-					
-					java.lang.System.out.println(String.valueOf(_blocks.size()));
-					
-					int from_y_top = (int) (current_block.getYOffset(next_jump_from
-							.getParent(), Block.TOP_POS) * _height) + getJBlock(true).getY();
-					int from_y_bottom = (int) (current_block.getYOffset(next_jump_from
-							.getParent(), Block.BOTTOM_POS) * _height) + getJBlock(true).getY();
-					
-					int to_x, to_y_top, to_y_bottom;
-					if (_score.outOfBlock(current_block, next_jump_to)) {
-						Block next_block = _blocks
-								.get(_current_block_index + 1);
-						
-						to_x = (int) (next_jump_to.getOffset() * getJBlock(
-								false).getImageWidth())
-								+ getJBlock(false).getImageOffset() - PAGE_LEFT;
-						
-						to_y_top = (int) (next_block.getYOffset(next_jump_to
-								.getParent(), Block.TOP_POS) * _height)
-								+ getJBlock(false).getY();
-						to_y_bottom = (int) (next_block.getYOffset(next_jump_to
-								.getParent(), Block.BOTTOM_POS) * _height)
-								+ getJBlock(false).getY();
-					} else {
-						
-						to_x = (int) (next_jump_to.getOffset() * getJBlock(
-								true).getImageWidth())
-								+ getJBlock(true).getImageOffset() - PAGE_LEFT;
-						
-						to_y_top = (int) (current_block.getYOffset(next_jump_to
-								.getParent(), Block.TOP_POS) * _height)
-								+ getJBlock(true).getY();
-						to_y_bottom = (int) (current_block.getYOffset(next_jump_to
-								.getParent(), Block.BOTTOM_POS) * _height)
-								+ getJBlock(true).getY();
-					}
-					// Current y values are referring to bottom of system
-					
-					int from_y, to_y;
-					if (to_y_bottom == from_y_bottom) {
-						from_y = from_y_bottom;
-						to_y = to_y_bottom;
-					}
-					else {
-						if (cal(next_jump_to, current_system, from_y_bottom, to_y_top, LENGTH) < cal(next_jump_to, current_system, from_y_top, to_y_bottom, LENGTH)) {
-							from_y = cal(next_jump_to, current_system, from_y_bottom, to_y_top, FROM);
-							to_y = cal(next_jump_to, current_system, from_y_bottom, to_y_top, TO);
-						}
-						else {
-							from_y = cal(next_jump_to, current_system, from_y_top, to_y_bottom, FROM);
-							to_y = cal(next_jump_to, current_system, from_y_top, to_y_bottom, TO);
-						}
-					}
-
-					_previous_jump_to = next_jump_to;
-				}
-
-			}
-
-			if (current_event.getEnd() == next_jump_from) {
-				current_block.makeJump(next_jump_from, next_jump_to);
-			}
-
-		}
-
-	}
-
-	// Get the current or the other JBlock using block index
-	private JBlock getJBlock(boolean isCurrent) {
-		if (_current_block_index % 2 == 0) {
-			if (isCurrent)
-				return _upper_block;
-			else
-				return _lower_block;
-		} else {
-			if (isCurrent)
-				return _lower_block;
-			else
-				return _upper_block;
-		}
-	}
-
 	private void moveCursor() {
 		if (_playback_events.isEmpty()) {
 			return;
@@ -578,15 +364,6 @@ public class DisplayPanel implements Panel, HcmpListener {
 			resetTime();
 			return;
 		}
-//		int playBackEventIndex = Math.min(_playback_events.size()-1, _events_index);
-//		PlaybackEvent playbackEvent = _playback_events.get(playBackEventIndex);
-//		Barline barline = _events_index < _playback_events.size() ? playbackEvent.getStart() : playbackEvent.getEnd();
-//		Block block = _blocks.get(_current_block_index);
-//		int x = (int) (barline.getOffset() * getJBlock(true)
-//				.getImageWidth()) + getJBlock(true).getImageOffset();
-//		int y = (int) (block.getYOffset(barline.getParent(), Block.BOTTOM_POS) * _height)
-//				+ getJBlock(true).getY();
-//		_cursor.setPosition(x, y - 5); // y-5: keep cursor inside block
 		redraw();
 	}
 
